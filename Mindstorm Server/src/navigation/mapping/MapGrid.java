e0c22ce7e140a10b9666c2b8b1155a119bd66721
FoxeI <storkner@googlemail.com>
2013-04-19 12:49:40 +0200

    Implemented get, set and clear methods for MapGrid

--- Mindstorm Server/src/navigation/mapping/MapGrid.java
+++ Mindstorm Server/src/navigation/mapping/MapGrid.java
@@ -1,12 +1,22 @@
 package navigation.mapping;
 
 public class MapGrid {
+	private final int G_SIZE = 128; // GridSize
+	private byte[][] grid;
 	
 	public MapGrid(){
-		
+		grid = new byte[G_SIZE][G_SIZE];
 	}
 	
-	public void addObstacle(int x, int y){
-		
+	public void set(int x, int y){
+		grid[x][y]++;
+	}
+	
+	public byte get(int x , int y){
+		return grid[x][y];
+	}
+	
+	public void clear(int x, int y){
+		grid[x][y] = 0;
 	}
 }
--- Mindstorm Server/src/navigation/mapping/Mapper.java
+++ Mindstorm Server/src/navigation/mapping/Mapper.java
@@ -1,18 +1,26 @@
 package navigation.mapping;
 
+import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
 
 public class Mapper {
 	private MapGrid mapGrid;
+	private LineMap lineMap;
 	
 	public Mapper(){
 		mapGrid = new MapGrid();
+		lineMap = new LineMap();
 	}
 	
 	public void addPose(Pose pose/*, ID id*/) {
 		  
 	}
-
+	
+	/**
+	 * 
+	 * @param pose robot pose
+	 * @param dist located obstacle distance
+	 */
 	public void addObstacle(Pose pose, int dist/*, ID id*/) {
 		//Todo: first check if its a other robot or a goal
 		
@@ -21,7 +29,19 @@ public class Mapper {
 		int ObstacleX = (int)pose.getX();
 		int ObstacleY = (int)pose.getY();
 		
-		mapGrid.addObstacle(ObstacleX, ObstacleY);
+		mapGrid.set(ObstacleX, ObstacleY);
+		buildLineMap();
+	}
+	
+	/**
+	 * generate a new LineMap
+	 */
+	private void buildLineMap(){
+		
+	}
+	
+	public LineMap getLineMap(){
+		return lineMap;
 	}
 	
 }
