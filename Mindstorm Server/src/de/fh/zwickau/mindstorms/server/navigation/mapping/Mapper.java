072dd4dbd53b54793f70a254887cef62b9c08a4c
Foxel <storkner@googlemail.com>
2013-04-21 15:56:36 +0200

    Implemented a manual method to add and remove obstacles from the GUI. Left mouse click to add and right click to remove.

--- Mindstorm Server/src/de/fh/zwickau/mindstorms/server/navigation/mapping/Mapper.java
+++ Mindstorm Server/src/de/fh/zwickau/mindstorms/server/navigation/mapping/Mapper.java
@@ -1,6 +1,5 @@
 package de.fh.zwickau.mindstorms.server.navigation.mapping;
 
-
 import de.fh.zwickau.mindstorms.server.view.View;
 import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
@@ -9,52 +8,67 @@ public class Mapper {
 	private View observer;
 	private MapGrid mapGrid;
 	private LineMap lineMap;
-	
-	public Mapper(){
+
+	public Mapper() {
 		mapGrid = new MapGrid();
 		lineMap = new LineMap();
 	}
-	
-	public void addPose(Pose pose/*, ID id*/) {
-		  
+
+	public void addPose(Pose pose/* , ID id */) {
+
+	}
+
+	/**
+	 * Add an Obstacle.
+	 * The Mapper will check if its a other robot or a goal.
+	 * @param pose robot pose 
+	 * @param dist located obstacle distance
+	 */
+	public void addObstacle(Pose pose, int dist/* , ID id */) {
+		// TODO: first check if its a other robot or a goal
+
+		// TODO: calculate the right coordinates
+		int ObstacleX = (int) pose.getX();
+		int ObstacleY = (int) pose.getY();
+
+		addObstacle(ObstacleX, ObstacleY);
 	}
 	
 	/**
-	 * 
-	 * @param pose robot pose
-	 * @param dist located obstacle distance
+	 * Add an Obstacle at the absolute position without
+	 * check if its a other robot or a goal.
+	 * @param x position
+	 * @param y position
 	 */
-	public void addObstacle(Pose pose, int dist/*, ID id*/) {
-		//TODO: first check if its a other robot or a goal
-		
-		
-		//TODO: calculate the right coordinates
-		int ObstacleX = (int)pose.getX();
-		int ObstacleY = (int)pose.getY();
-		
-		mapGrid.set(ObstacleX, ObstacleY);
+	public void addObstacle(int x, int y){
+		mapGrid.set(x, y);
 		buildLineMap();
 		observer.mapChanged();
 	}
 	
+	public void removeObstacle(int x, int y){
+		mapGrid.clear(x, y);
+		buildLineMap();
+		observer.mapChanged();
+	}
+
 	/**
 	 * generate a new LineMap
 	 */
-	private void buildLineMap(){
+	private void buildLineMap() {
 		
 	}
-	
-	public MapGrid getGrid(){
+
+	public MapGrid getGrid() {
 		return mapGrid;
 	}
-	
-	public LineMap getLineMap(){
+
+	public LineMap getLineMap() {
 		return lineMap;
 	}
-	
-	public void setObserverView(View observer){
+
+	public void setObserverView(View observer) {
 		this.observer = observer;
 	}
-	
-	
+
 }
--- Mindstorm Server/src/de/fh/zwickau/mindstorms/server/view/View.java
+++ Mindstorm Server/src/de/fh/zwickau/mindstorms/server/view/View.java
@@ -3,6 +3,7 @@ package de.fh.zwickau.mindstorms.server.view;
 import java.util.concurrent.Semaphore;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
@@ -10,144 +11,175 @@ import de.fh.zwickau.mindstorms.server.navigation.mapping.MapGrid;
 import de.fh.zwickau.mindstorms.server.navigation.mapping.Mapper;
 import static org.lwjgl.opengl.GL11.*;
 
-public class View extends Thread{
-	
+public class View extends Thread {
+
 	private Mapper mapper;
 	private boolean mapChanged;
 	private Semaphore semaphore;
-	
-	//Draw Items
+
+	// Draw Items
 	private float[] vertices;
-	private float[]	colors;
-	
-	public View(){
+	private float[] colors;
+
+	public View() {
 		this.mapChanged = true;
 		this.semaphore = new Semaphore(1);
 	}
 
 	@Override
-	public void run(){
+	public void run() {
 		createWindow();
 		initialize();
-		
-		while(!Display.isCloseRequested()){
-			if(mapChanged) {
-				try { 
-					rebuildMap();
-				} catch (InterruptedException e) {
-					//then use the old one...
-				}
-			}
+
+		while (!Display.isCloseRequested()) {
+			update();
 			draw();
 		}
-		
+
 		Display.destroy();
 	}
-	
+
 	/**
 	 * Create the View Window
 	 */
 	private void createWindow() {
 		try {
-			Display.setDisplayMode(new DisplayMode(512+1,512+1));
+			Display.setDisplayMode(new DisplayMode(512 + 1, 512 + 1));
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			System.err.println("Failed to create an OpenGL Window :(");
 		}
-		
-		
+
 	}
-	
+
 	/**
 	 * Initialize some OpenGL parameters and allocate memory
 	 */
-	private void initialize(){
+	private void initialize() {
 		final int size = mapper.getGrid().getGridSize();
-		
-		//GL initialize
-		glPointSize(Display.getWidth() / size -1);
-		
-		//initialize pixel grid
+
+		// GL initialize
+		glPointSize(Display.getWidth() / size - 1);
+
+		// initialize pixel grid
 		vertices = new float[size * size * 2];
 		colors = new float[size * size * 3];
-		
+
 		int i = -1;
 		int c = -1;
 		float offset = -1.0f + 1.0f / size;
-		for(int x = 0; x < size; x++){
-			for(int y = 0; y < size; y++){
-				vertices[++i] = x/((float)size / 2.0f) + offset; //X
-				vertices[++i] = y/((float)size / 2.0f) + offset; //Y
-				
-				colors[++c] = 0.5f; //red
-				colors[++c] = 0.5f; //green
-				colors[++c] = 0.5f; //blue
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				vertices[++i] = x / ((float) size / 2.0f) + offset; // X
+				vertices[++i] = y / ((float) size / 2.0f) + offset; // Y
+
+				colors[++c] = 0.5f; // red
+				colors[++c] = 0.5f; // green
+				colors[++c] = 0.5f; // blue
 			}
 		}
 	}
-	
+
 	/**
-	 * Update the colors of the MapGrid. 
-	 * Green = clear area
-	 * Red	 = area with located obstacle
+	 * Update the colors of the MapGrid. Green = clear area Red = area with
+	 * located obstacle
+	 * 
 	 * @throws InterruptedException
 	 */
-	private void rebuildMap() throws InterruptedException{
+	private void rebuildMap() throws InterruptedException {
 		semaphore.acquire();
 		mapChanged = false;
 		semaphore.release();
-		
+
 		MapGrid grid = mapper.getGrid();
 		final int size = grid.getGridSize();
-		
+
 		int c = -1;
-		for(int x = 0; x < size; x++){
-			for(int y = 0; y < size; y++){
-				if(grid.get(x, y) > 0){
-					colors[++c] = 1.0f;	//red
-					colors[++c] = 0.0f;	//green
-					colors[++c] = 0.0f; //blue
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				if (grid.get(x, y) > 0) {
+					colors[++c] = 1.0f; // red
+					colors[++c] = 0.0f; // green
+					colors[++c] = 0.0f; // blue
 				} else {
-					colors[++c] = 0.0f; //red
-					colors[++c] = 1.0f; //green
-					colors[++c] = 0.0f; //blue
+					colors[++c] = 0.0f; // red
+					colors[++c] = 1.0f; // green
+					colors[++c] = 0.0f; // blue
 				}
 			}
 		}
+	}
+
+	private void update(){
+		//Map update if it was changed
+		if (mapChanged) {
+			try {
+				rebuildMap();
+			} catch (InterruptedException e) {
+				// then use the old one...
+			}
+		}
+		
+		//Handle User Inputs
+		boolean l_Button = Mouse.isButtonDown(0); //left
+		boolean r_Button = Mouse.isButtonDown(1); //right
+		
+		if(l_Button){	// create a Obstacle
+			int view_offset = Display.getWidth() / mapper.getGrid().getGridSize();
+			
+			int X = Mouse.getX() / view_offset;
+			int Y = Mouse.getY() / view_offset;
+			
+			mapper.addObstacle(X, Y);
+		}
+		
+		if(r_Button){	// remove a Obstacle
+			int view_offset = Display.getWidth() / mapper.getGrid().getGridSize();
+			
+			int X = Mouse.getX() / view_offset;
+			int Y = Mouse.getY() / view_offset;
+			
+			mapper.removeObstacle(X, Y);
+		}
+		
 	}
 	
 	/**
 	 * Draw all data on OpenGL canvas.
 	 */
-	private void draw(){
-		int size = vertices.length -1;
+	private void draw() {
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		
+		int size = vertices.length - 1;
 		int i = -1;
 		int c = -1;
-		
-		glBegin(GL_POINTS);										//Begin to draw Points
-		while(i < size){
-			glColor3f(colors[++c], colors[++c], colors[++c]); 	//set pixel color
-			glVertex2f(vertices[++i], vertices[++i]);			//make a point
+
+		glBegin(GL_POINTS); // Begin to draw Points
+		while (i < size) {
+			glColor3f(colors[++c], colors[++c], colors[++c]);	// set pixel color
+			glVertex2f(vertices[++i], vertices[++i]); 			// make a point
 		}
-		glEnd();												//End with draw
-		
-		Display.update();										//Bring it to the screen.		
+		glEnd(); 												// End with draw
+
+		Display.update(); 										// Bring it to the screen.
 	}
-	
+
 	/**
 	 * Register the Mapper to be observed.
+	 * 
 	 * @param mapper
 	 */
-	public void registerMapper(Mapper mapper){
+	public void registerMapper(Mapper mapper) {
 		this.mapper = mapper;
 	}
-	
+
 	/**
 	 * Tells view that the map has changed (Thread safe)
 	 */
-	public void mapChanged(){
-		
+	public void mapChanged() {
+
 		try {
 			semaphore.acquire();
 		} catch (InterruptedException e) {
