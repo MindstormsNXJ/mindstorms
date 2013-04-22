1afbe1ce7a7d33c0cfbd9a4891881a123da235e9
Cookieforge <pr.geld@web.de>
2013-04-22 19:03:45 +0200

    Prepare a method to convert a Grid to LineMap

--- Mindstorm Server/src/de/fh/zwickau/mindstorms/server/navigation/mapping/Converter.java
+++ Mindstorm Server/src/de/fh/zwickau/mindstorms/server/navigation/mapping/Converter.java
@@ -2,6 +2,7 @@ package de.fh.zwickau.mindstorms.server.navigation.mapping;
 
 import static java.lang.Math.*;
 
+import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
 
 /**
@@ -24,5 +25,49 @@ public class Converter {
 		target[1] = (float)(cos(toRadians((double)pose.getHeading())) * (double)dist + pose.getY());
 		
 		return target;
+	}
+	
+	public static LineMap gridToLineMap(MapGrid grid) {
+		
+		final int size = grid.getGridSize();
+		
+		for(int x = 0; x < size; x++) {
+			for(int y = 0; y < size; y++) {
+				
+				if(grid.get(x, y) == 0) {
+					
+					if(x != size -1){
+						if(grid.get(x + 1, y) > 0) {
+							
+						}
+					}
+					
+					if(x != 0){
+						if(grid.get(x - 1, y) > 0) {
+							
+						}
+					}
+					
+					if(y != size -1){
+						if(grid.get(x, y + 1) > 0) {
+							
+						}
+					}
+					if(y != 0){
+						if(grid.get(x, y - 1) > 0) {
+							
+						}
+					}
+				}
+				
+				
+				
+			}
+			
+		}
+		
+		
+		return null;
+		
 	}
 }
