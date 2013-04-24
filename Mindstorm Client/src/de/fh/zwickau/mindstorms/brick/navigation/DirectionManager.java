8caa272dbd8537f8fc43b86481e9453998886ce5
Aismael <aismaelinc@web.de>
2013-04-24 16:23:04 +0200

    direction manager rebuild

--- Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/navigation/DirectionManager.java
+++ Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/navigation/DirectionManager.java
@@ -8,10 +8,10 @@ public class DirectionManager implements Manager{
 	int motSpeed = 200;// Speed how fast the Robot should Rotate
 	private Robot robot;// the Robot who is to rotate
 	private boolean rotate;// boolean if robot is rotating
-	private float startDirection;// the direction where the robot stands at
+	private int startDirection;// the direction where the robot stands at
 									// start
-	private float directioner;// the direction where the Robot should move
-	private float degrees;// the dergree what to move
+	private int directioner;// the direction where the Robot should move
+	private int degrees;// the dergree what to move
 	private int stepWide = 45;// the stepwide for stepwise rotating
 
 	/**
@@ -29,17 +29,17 @@ public class DirectionManager implements Manager{
 	 * @param deg
 	 * @param dir
 	 */
-	public void rotateInDirection(int deg, Direction dir) {
+	public void rotateInDirection(int deg, direction dir) {
 		rotate = true;
-		startDirection = robot.compassSensor.getDegrees();
-		if (dir == Direction.RIGHT) {
+		startDirection = (int) robot.compassSensor.getDegrees();
+		if (dir == direction.RIGHT) {
 			robot.leftMotor.forward();
 			robot.rightMotor.backward();
 			degrees = deg;
 		}
-		if (dir == Direction.LEFT) {
-			robot.leftMotor.forward();
-			robot.rightMotor.backward();
+		if (dir == direction.LEFT) {
+			robot.leftMotor.backward();
+			robot.rightMotor.forward();
 			degrees = -deg;
 		}
 		Thread check = new Thread(new Runnable() {
@@ -47,8 +47,8 @@ public class DirectionManager implements Manager{
 			@Override
 			public void run() {
 				while (rotate == true) {
-					directioner = robot.compassSensor.getDegrees();
-					if (directioner == startDirection + degrees % 360) {
+					directioner = (int) robot.compassSensor.getDegrees();
+					if (directioner ==( startDirection + degrees) % 360) {
 						rotate = false;
 					}
 				}
