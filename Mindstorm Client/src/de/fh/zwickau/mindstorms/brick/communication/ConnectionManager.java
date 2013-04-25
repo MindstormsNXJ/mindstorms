d2180c30a63f60376492ff634d9d95e15b69a035
tschiessl <tos111j8@fh-zwickau.de>
2013-04-25 15:07:06 +0200

    improved ConnectionManager: established connection to server now and initialises the streams; sendPose should work now (parses pose and sends it via stream)

--- Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/communication/ConnectionManager.java
+++ Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/communication/ConnectionManager.java
@@ -1,23 +1,82 @@
 package de.fh.zwickau.mindstorms.brick.communication;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.USB;
 import lejos.robotics.navigation.Pose;
+import lejos.util.Delay;
 import de.fh.zwickau.mindstorms.brick.Robot;
 
 public class ConnectionManager {
 	
 	private Robot robot;
+	private DataOutputStream positionWriter;
+	private DataInputStream commandReceiver;
 	
 	public ConnectionManager(Robot robot) {
 		this.robot = robot;
 		establishConnection();
-	}
-	
-	public void sendPose(Pose pose) {
-		//TODO
+		waitForCommands();
 	}
 	
 	private void establishConnection() {
+		NXTConnection connection = USB.waitForConnection();
+//		NXTConnection connection = Bluetooth.waitForConnection();
 		
+		positionWriter = connection.openDataOutputStream();
+		commandReceiver = connection.openDataInputStream();
+	}
+	
+	private void waitForCommands() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						while (commandReceiver.available() == 0)
+							Delay.msDelay(100);
+						String command = commandReceiver.readUTF();
+						parseCommand(command);
+					} catch (IOException e) {
+						
+					}
+				}
+			}
+			
+		}).start();
+	}
+	
+	private void parseCommand(String command) {
+		
+	}
+	
+	public boolean sendPose(Pose pose) {
+		boolean success = false;
+		String parsedPose = parsePose(pose);
+		try {
+			positionWriter.writeUTF(parsedPose);
+			success = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
+	
+	private String parsePose(Pose poseToParse) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("x");
+		builder.append(poseToParse.getX());
+		builder.append("y");
+		builder.append(poseToParse.getY());
+		builder.append("dir");
+		builder.append(poseToParse.getHeading());
+		builder.append("end");
+		return builder.toString();
 	}
 	
 }
