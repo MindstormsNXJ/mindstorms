8e8323eb19353455a2491a4f68e7532f962ddda5
Heliosana <krummnacker.m@gmail.com>
2013-05-06 13:59:21 +0200

    scan left right implemented

--- Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/sensors/ObjectCentralisation.java
+++ Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/sensors/ObjectCentralisation.java
@@ -4,37 +4,46 @@ import de.fh.zwickau.mindstorms.brick.Robot;
 import de.fh.zwickau.mindstorms.brick.navigation.Direction;
 
 public class ObjectCentralisation {
-	
+
 	private Robot robot;
 	private int distance;
 	private boolean isdetected;
 
 	public ObjectCentralisation(Robot robot) {
-		this.robot=robot;
-		int targetAngel = calcAngel(scanLeft(),scanRight());
+		this.robot = robot;
+		int targetAngel = calcAngel(scanLeft(), scanRight());
 		System.out.println(targetAngel);
 		robot.positionManager.rotateTo(targetAngel);
 	}
-	
+
 	private int calcAngel(int leftAngel, int rightAngel) {
 		int centralAngel = 0;
-		centralAngel= (leftAngel+rightAngel)/2;
+		centralAngel = (leftAngel + rightAngel) / 2;
 		if (leftAngel > rightAngel) {
-			centralAngel+=180;
+			centralAngel += 180;
 		}
 		return centralAngel;
 	}
 
 	private int scanRight() {
-		distance= robot.ultrasonicSensor.getDistance();
+		distance = robot.ultrasonicSensor.getDistance();
 		while (isdetected) {
-		robot.positionManager.rotate(3, Direction.LEFT);
+			robot.positionManager.rotate(3, Direction.LEFT);
+		}
+		while (!isdetected) {
+			robot.positionManager.rotate(1, Direction.RIGHT);
 		}
 		return (int) robot.compassSensor.getDegrees();
-		}
+	}
 
 	private int scanLeft() {
-		// TODO Auto-generated method stub	
+		distance = robot.ultrasonicSensor.getDistance();
+		while (isdetected) {
+			robot.positionManager.rotate(3, Direction.RIGHT);
+		}
+		while (!isdetected) {
+			robot.positionManager.rotate(1, Direction.LEFT);
+		}
 		return (int) robot.compassSensor.getDegrees();
 	}
 }
