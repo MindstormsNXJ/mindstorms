898d2ee50bd26d4ef023c8673e1a2d47cd66069b
FoxeI <storkner@googlemail.com>
2013-05-18 11:52:23 +0200

    Initial-Branch -- Added Class Camera

---
+++ Mindstorm Server/src/de/fh/zwickau/mindstorms/server/navigation/mapping/camera/Camera.java
@@ -0,0 +1,13 @@
+package de.fh.zwickau.mindstorms.server.navigation.mapping.camera;
+
+public class Camera {
+    
+    
+    public Camera(){
+        
+    }
+    
+    public void makeNewPhoto(){
+        
+    }
+}
