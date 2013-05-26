5d0b54f9941935176818b023432dcdec5ec1be7a
tschiessl <tos111j8@fh-zwickau.de>
2013-05-26 13:50:54 +0200

    added comments to MyObjectCentralisation

--- Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/task/MyObjectCentralisation.java
+++ Mindstorm Client/src/de/fh/zwickau/mindstorms/brick/task/MyObjectCentralisation.java
@@ -4,17 +4,32 @@ import lejos.util.Delay;
 import de.fh.zwickau.mindstorms.brick.Robot;
 import de.fh.zwickau.mindstorms.brick.navigation.Direction;
 
+/**
+ * This class is my proposal for object centralisation which should be done before picking and 
+ * dropping the ball.
+ * 
+ * @author Tobias Schießl
+ * @version 1.0
+ */
 public class MyObjectCentralisation {
 
 	private Robot robot;
 	private int distance;
-	private boolean isdetected = true;
+	private boolean isdetected;
 	private int startAngle;
 
+	/**
+	 * Initialises a new ObjectCentralisation and starts the centralisation process.
+	 * At the end, the robot will have the target directly in front.
+	 * 
+	 * @param robot the robot with the sensors and position manager
+	 * @throws IllegalStateException if there is no object to centralise on
+	 */
 	public MyObjectCentralisation(Robot robot) throws IllegalStateException {
 		this.robot = robot;
 		startAngle = (int) robot.compassSensor.getDegrees();
 		initDistance();
+		isdetected = true;
 		int leftBorder = scanLeft();
 		reset();
 		int rightBorder = scanRight();
@@ -23,6 +38,12 @@ public class MyObjectCentralisation {
 		robot.positionManager.rotateTo(targetAngle);
 	}
 	
+	/**
+	 * Tries to initialise the distance to the target.
+	 * Note, that if the distance is still 255 after 10 seconds, an IllegalStateException will be thrown.
+	 * 
+	 * @throws IllegalStateException if no object could be detected in front of the robot
+	 */
 	private void initDistance() throws IllegalStateException {
 		long startTime = System.currentTimeMillis();
 		while ((distance = robot.ultrasonicSensor.getDistance()) == 255) {
@@ -33,11 +54,21 @@ public class MyObjectCentralisation {
 		System.out.println(distance);
 	}
 	
+	/**
+	 * Resets the robot to it's initial position and set's the isDetected flag to true.
+	 */
 	private void reset() {
 		robot.positionManager.rotateTo(startAngle);
 		isdetected = true;
 	}
 
+	/**
+	 * Calculates the target angle from the left and right border.
+	 * 
+	 * @param leftAngle the left border angle
+	 * @param rightAngle the right border angle
+	 * @return the target angle
+	 */
 	private int calcAngle(int leftAngle, int rightAngle) {
 		int centralAngle = 0;
 		centralAngle = (leftAngle + rightAngle) / 2;
@@ -46,6 +77,11 @@ public class MyObjectCentralisation {
 		return centralAngle;
 	}
 
+	/**
+	 * Finds the right border angle of the object.
+	 * 
+	 * @return the right border angle.
+	 */
 	private int scanRight() {
 		while (isdetected) {
 			robot.positionManager.rotate(3, Direction.RIGHT);
@@ -59,6 +95,11 @@ public class MyObjectCentralisation {
 		return rightBorderAngle;
 	}
 
+	/**
+	 * Finds the left border angle of the object.
+	 * 
+	 * @return the left border angle
+	 */
 	private int scanLeft() {
 		while (isdetected) {
 			robot.positionManager.rotate(3, Direction.LEFT);
@@ -72,6 +113,12 @@ public class MyObjectCentralisation {
 		return leftBorderAngle;
 	}
 	
+	/**
+	 * Checks, if the object is really out of the ultrasonic sensors range.
+	 * Therefore, the robot will stop and take 10 values, from whom 5 must be out of range.
+	 * 
+	 * @return false, if the object is no longer in the ultrasonic sensors range
+	 */
 	private boolean checkReallyOutOfRange() {
 		int out = 0;
 		for (int i = 0; i < 10; ++i) {
